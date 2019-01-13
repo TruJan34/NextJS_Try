71d6bc53eb6b5a457836f6910474740b28bb20ec
TruJan34 <trujan34@gmail.com>
2019-01-13 18:18:24 +0530

    nodemon config express backend with routes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 // import Link from 'next/link';
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 import '../scss/style.scss';
 
 const Index = () => (
   <section>
+    <Head>
+      <title>Hello world</title>
+    </Head>
     <Navbar />
     <h1>Hello World from Next Js</h1>
     {/* <Link href="/about">
